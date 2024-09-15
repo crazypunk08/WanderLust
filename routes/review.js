90d8841e5e51e87d59eae8b8d99f7f143035578e
crazypunk08 <naren.lakamannavar@gmail.com>
2024-09-15 11:27:55 +0530

    Fixed review logic

--- routes/review.js
+++ routes/review.js
@@ -22,7 +22,7 @@ const validatereview = (req, res, next) => {
 router.post("/reviews",isLoggedIn,validatereview,wrapAsync(reviewController.createReview));
 
 //Delete a review route
-router.delete("/reviews/:reviewId",isLoggedIn,wrapAsync(reviewController.deleteReview));
-// ,isreviewOwner
+router.delete("/reviews/:reviewId",isLoggedIn,isreviewOwner,wrapAsync(reviewController.deleteReview));
+// 
 
 module.exports=router;
